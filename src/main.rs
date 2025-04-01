f5d67abb75c16c29cf73dfc61b2b2c2db196f28c
zabackary <137591653+zabackary@users.noreply.github.com>
2025-04-01 16:41:50 +0900

    feat: add dash touch support

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,118 @@
-use iced::Task;
+use iced::{Settings, Task};
+use screenshot::{MouseEvent, ScreenshotStreamEvent};
 
 mod screenshot;
 
 const V5_BRAIN_RAW_SCREEN_SIZE: (u32, u32) = (512, 272);
 const V5_BRAIN_SCREEN_SIZE: (u32, u32) = (480, 272);
 
+#[derive(Debug, Clone)]
+enum MouseUpdate {
+    Press,
+    Release,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Screenshot(iced::widget::image::Handle),
+    Mouse(MouseUpdate),
+    MouseMove(u16, u16),
+    MouseEventSender(tokio::sync::mpsc::Sender<MouseEvent>),
+}
+
 struct V5BrainStream {
     image: iced::widget::image::Handle,
+    mouse_event_sender: Option<tokio::sync::mpsc::Sender<MouseEvent>>,
+    mouse_position: (u16, u16),
+    mouse_pressed: bool,
 }
 
 impl Default for V5BrainStream {
     fn default() -> Self {
         let image = iced::widget::image::Handle::from_rgba(512, 272, vec![0; 512 * 272 * 4]);
 
-        V5BrainStream { image }
+        V5BrainStream {
+            image,
+            mouse_event_sender: None,
+            mouse_position: (0, 0),
+            mouse_pressed: false,
+        }
     }
 }
 
 impl V5BrainStream {
-    fn update(
-        &mut self,
-        message: iced::widget::image::Handle,
-    ) -> iced::Task<iced::widget::image::Handle> {
-        self.image = message;
+    fn update(&mut self, message: Message) -> iced::Task<Message> {
+        match message {
+            Message::Screenshot(image) => {
+                self.image = image;
+            }
+            Message::MouseMove(x, y) => {
+                self.mouse_position = (x, y);
+                if self.mouse_pressed {
+                    if let Some(sender) = &self.mouse_event_sender {
+                        sender
+                            .try_send(MouseEvent {
+                                x,
+                                y,
+                                pressing: true,
+                            })
+                            .unwrap_or_else(|_| {
+                                eprintln!("Failed to send mouse event: {:?}", message);
+                            });
+                    } else {
+                        eprintln!("Mouse event sender not initialized");
+                    }
+                }
+            }
+            Message::Mouse(event) => {
+                if let Some(sender) = &self.mouse_event_sender {
+                    sender
+                        .try_send(match event {
+                            MouseUpdate::Press => {
+                                self.mouse_pressed = true;
+                                MouseEvent {
+                                    x: self.mouse_position.0,
+                                    y: self.mouse_position.1,
+                                    pressing: true,
+                                }
+                            }
+                            MouseUpdate::Release => {
+                                self.mouse_pressed = false;
+                                MouseEvent {
+                                    x: self.mouse_position.0,
+                                    y: self.mouse_position.1,
+                                    pressing: false,
+                                }
+                            }
+                        })
+                        .unwrap_or_else(|_| {
+                            eprintln!("Failed to send mouse event: {:?}", event);
+                        });
+                } else {
+                    eprintln!("Mouse event sender not initialized");
+                }
+            }
+
+            Message::MouseEventSender(sender) => {
+                self.mouse_event_sender = Some(sender);
+            }
+        }
         Task::none()
     }
 
-    fn view(&self) -> iced::Element<'_, iced::widget::image::Handle> {
-        iced::widget::image(self.image.clone()).into()
+    fn view(&self) -> iced::Element<'_, Message> {
+        iced::widget::mouse_area(iced::widget::image(self.image.clone()))
+            .on_press(Message::Mouse(MouseUpdate::Press))
+            .on_release(Message::Mouse(MouseUpdate::Release))
+            .on_move(|pos| Message::MouseMove(pos.x as u16, pos.y as u16))
+            .into()
     }
 
-    fn subscription(&self) -> iced::Subscription<iced::widget::image::Handle> {
-        iced::Subscription::run(|| screenshot::screenshot_stream())
+    fn subscription(&self) -> iced::Subscription<Message> {
+        iced::Subscription::run(screenshot::screenshot_stream).map(|event| match event {
+            ScreenshotStreamEvent::Image(image) => Message::Screenshot(image),
+            ScreenshotStreamEvent::Sender(sender) => Message::MouseEventSender(sender),
+        })
     }
 }
 
--- src/screenshot.rs
+++ src/screenshot.rs
@@ -4,17 +4,38 @@ use iced::futures::SinkExt;
 use iced::{futures::Stream, stream};
 use image::{GenericImageView, RgbaImage};
 use tokio::task::spawn_blocking;
+use tokio::time::sleep;
 use vex_v5_serial::commands::file::DownloadFile;
 use vex_v5_serial::connection::serial::{self};
 use vex_v5_serial::connection::Connection;
+use vex_v5_serial::packets;
 use vex_v5_serial::packets::capture::{ScreenCapturePacket, ScreenCaptureReplyPacket};
 use vex_v5_serial::packets::file::{FileTransferTarget, FileVendor};
 use vex_v5_serial::string::FixedString;
 
 use crate::{V5_BRAIN_RAW_SCREEN_SIZE, V5_BRAIN_SCREEN_SIZE};
 
-pub fn screenshot_stream() -> impl Stream<Item = iced::widget::image::Handle> {
+#[derive(Debug, Clone, Copy)]
+pub struct MouseEvent {
+    pub x: u16,
+    pub y: u16,
+    pub pressing: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum ScreenshotStreamEvent {
+    Image(iced::widget::image::Handle),
+    Sender(tokio::sync::mpsc::Sender<MouseEvent>),
+}
+
+pub fn screenshot_stream() -> impl Stream<Item = ScreenshotStreamEvent> {
     stream::channel(10, |mut output| async move {
+        let (mouse_event_sender, mut mouse_event_receiver) = tokio::sync::mpsc::channel(200);
+        output
+            .send(ScreenshotStreamEvent::Sender(mouse_event_sender))
+            .await
+            .unwrap();
+
         // Find all vex devices on serial ports.
         let devices = serial::find_devices().expect("failed to enumerate devices");
 
@@ -30,17 +51,39 @@ pub fn screenshot_stream() -> impl Stream<Item = iced::widget::image::Handle> {
         .unwrap();
 
         loop {
+            while let Ok(event) = mouse_event_receiver.try_recv() {
+                println!("Sending mouse event: {:?}", event);
+                // Send the mouse event to the brain
+                connection
+                    .packet_handshake::<packets::dash::SendDashTouchReplyPacket>(
+                        Duration::from_millis(100),
+                        5,
+                        packets::dash::SendDashTouchPacket::new(
+                            packets::dash::SendDashTouchPayload {
+                                x: event.x,
+                                y: event.y,
+                                pressing: event.pressing as u16,
+                            },
+                        ),
+                    )
+                    .await
+                    .expect("failed to send dash touch packet");
+                // Allow the brain to process the event
+                sleep(Duration::from_millis(100)).await;
+            }
+
             // Stolen from https://github.com/vexide/cargo-v5/blob/667a90226a1400613ac1a75c0c3191974d0a1f32/src/commands/screenshot.rs#L26
 
             // Tell the brain we want to take a screenshot
             if let Err(_) = connection
                 .packet_handshake::<ScreenCaptureReplyPacket>(
                     Duration::from_millis(100),
-                    5,
+                    1,
                     ScreenCapturePacket::new(()),
                 )
                 .await
             {
+                eprintln!("Failed to send screen capture packet");
                 break;
             }
 
@@ -83,10 +126,12 @@ pub fn screenshot_stream() -> impl Stream<Item = iced::widget::image::Handle> {
                     .to_image();
 
             output
-                .send(iced::widget::image::Handle::from_rgba(
-                    image.width(),
-                    image.height(),
-                    image.into_raw(),
+                .send(ScreenshotStreamEvent::Image(
+                    iced::widget::image::Handle::from_rgba(
+                        image.width(),
+                        image.height(),
+                        image.into_raw(),
+                    ),
                 ))
                 .await
                 .expect("couldn't send image back");
